6eefe59b496b56181884435f8afbb0e8b66613d7
Maksim0403 <maksim.didychuk4@gmail.com>
2025-11-17 12:57:50 +0200

    initialize project bootstrap and root component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+</script>
+
+<template>
+  <div id="app">
+    <AppHeader />
+
+    <main class="main-content">
+      <router-view :key="$route.path" />
+    </main>
+
+    <FooterComponent />
+  </div>
+</template>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+}
+</style>
