c1775b5412e1b7941f4a527df758bf32d4d4b817
Vlad Buzhdyhan <vladyslavbuzhdyhan@gmail.com>
2025-11-17 12:56:12 +0200

    configure application routing

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/views/CartView.vue')
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('@/views/FavoritesView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/NotFoundView.vue')
+    },
+  ],
+
+  scrollBehavior() {
+    return {top: 0}
+  }
+})
+
+export default router
